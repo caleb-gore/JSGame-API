bd82f969d045d21fd3e8099e5a397bff882681d1
Caleb Gore <cgore2001@gmail.com>
2022-09-13 14:23:58 -0500

    auth and game views

--- JSGame/urls.py
+++ JSGame/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from JSGame_api.views import register_user, login_user, GameView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'games', GameView, 'game')
 
 urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
---
+++ JSGame_api/views/game.py
@@ -0,0 +1,55 @@
+""" View module for handling request about games """
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from JSGame_api.models import Game
+
+class GameSerializer(serializers.ModelSerializer):
+    """ JSON serializer for games """
+    
+    class Meta:
+        model = Game
+        fields = (
+            'id',
+            'score',
+            'level',
+            'date_created',
+            'last_saved',
+            'user',
+            'lives',
+            'game_over'
+        )
+
+class GameView(ViewSet):
+    """ JSGame games view """
+    
+    def create(self, request):
+        """ Handle POST operations 
+        
+        Returns
+            Response -- JSON serialized game instance
+        """
+        
+        
+        game = Game.objects.create(
+            score = request.data["score"],
+            level = request.data["level"],
+            lives = request.data["lives"],
+            user = request.auth.user,
+            game_over = request.data["game_over"]
+        )
+        
+        serializer = GameSerializer(game)
+        return Response(serializer.data)
+    
+    def list(self, request):
+        """Handle GET requests to get all games
+        Returns:
+            Response -- JSON serialized list of games
+        """
+        games = Game.objects.all()
+        games = games.filter(user=request.auth.user)
+
+        serializer = GameSerializer(games, many=True)
+        return Response(serializer.data)
